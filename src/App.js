88a19018aae66bccfabbe6133b9ccbc7e5054262
Muhammad Risano Akbar2 <rinoboy84@gmail.com>
2019-07-20 16:52:50 +0700

    muhammad

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-=======
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom'
 import books from './Screens/Books';
@@ -110,5 +83,4 @@ class App extends Component {
 
 
 
->>>>>>> ee71556c0e1fecf253c339aafd40b5092c8d56a9
 export default App;
